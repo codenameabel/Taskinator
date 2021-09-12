c18773f4d3efff3a622782a7432e40af7c5b7305
codenameabel <newabelmartinez@gmail.com>
2021-09-12 12:33:45 -0700

    completed js feature to be able to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
-
+var createTaskHandler = function (event) {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // CREATE LIST ITEM
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+
+    // CREATE DIV TO HOLD TASK INFO AND ADD TO LIST ITEM
+    var taskInfoEl = document.createElement("div");
+
+    // GIVE IT A CLASS NAME 
+    taskInfoEl.className = "task-info";
+
+    // ADD HTML CONTENT TO DIV 
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // ADD ENTIRE LIST ITEM TO LIST
+    tasksToDoEl.appendChild(listItemEl)
+
 };
 
-formEl.addEventListener("submit", createTaskHandler); 
- 
+formEl.addEventListener("submit", createTaskHandler);
